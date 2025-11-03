8f948ddb9ddf42de3ab7c248d758b707e30be32b
Thato Tshoma <Thato.Tshoma@sita.co.za>
2025-11-03 18:37:47 +0200

    Created Usercollection

---
+++ E_Prescribing_API/CollectionModel/UserCollection.cs
@@ -0,0 +1,15 @@
+﻿using E_Prescribing_API.Data.Services;
+using E_Prescribing_API.Models;
+
+namespace E_Prescribing_API.CollectionModel
+{
+    public class UserCollection
+    {
+        public ApplicationUser ApplicationUsers { get; set; }
+        public Nurse Nurse { get; set; }
+        public Pharmacist Pharmacist { get; set; }
+        public Surgeon Surgeon { get; set; }
+        public Anaesthesiologist Anaesthesiologist { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ E_Prescribing_API/Models/Pharmacist.cs
@@ -0,0 +1,24 @@
+﻿using E_Prescribing_API.Data.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Prescribing_API.Models
+{
+    public class Pharmacist
+    {
+        [Key]
+        public int PharmacistId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+        [Display(Name = "Contact Number")]
+        public string ContactNumber { get; set; }
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+        [Display(Name = "Registration Number")]
+        public string RegistrationNumber { get; set; }
+        public ApplicationUser User { get; set; }
+        public int UserId { get; set; }
+    }
+}
