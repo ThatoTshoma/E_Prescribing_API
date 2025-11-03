9dfbf15531fa72718988c9753b672e7aa3e07d5c
Thato Tshoma <Thato.Tshoma@sita.co.za>
2025-11-03 19:03:18 +0200

    Created Username generator

---
+++ E_Prescribing_API/Data/Services/UsernameGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Prescribing_API.Data.Services
+{
+    public class UsernameGenerator
+    {
+        public string GenerateUserName(string email)
+        {
+            return email.Split('@')[0];
+        }
+    }
+}
