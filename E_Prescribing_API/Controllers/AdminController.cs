692686cfd9226359d71897759cc8386af6555adc
Thato Tshoma <Thato.Tshoma@sita.co.za>
2025-11-03 18:58:25 +0200

    Created Password generator

---
+++ E_Prescribing_API/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using E_Prescribing_API.CollectionModel;
+using E_Prescribing_API.Data.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Prescribing_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _db;
+        private readonly IEmailSender _emailSender;
+
+        public AdminController(UserManager<ApplicationUser> userManager, ApplicationDbContext db, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _db = db;
+            _emailSender = emailSender;
+        }
+
+
+        public IActionResult AddUser()
+        {
+            var userCollection = new UserCollection();
+
+            return BadRequest(new { message = "Username or password is incorrect." });
+        }
+
+
+    }
+}
